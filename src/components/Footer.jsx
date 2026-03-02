3dfd2eede2ddb05169b5a8587cd12798f09f8aae
Mayra Alzate <nadja.minni@gmail.com>
2026-03-01 22:07:39 -0500

    Componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <div>
+                <p>&copy; 2026 Sazamajo Inc.Todos los derechos reservados.</p>
+                <p>Contáctanos: <a href="">info@sazamajo.com </a></p>
+                <ul>
+                    <li><a href="#">Política de Privacidad</a></li>
+                    <li><a href="#">Términos de Uso</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
