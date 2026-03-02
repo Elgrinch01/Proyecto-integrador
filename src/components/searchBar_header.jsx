298a332c1be54f9f1d855784621423caa398ae29
Elgrinch001 <santyelmejor105@gmail.com>
2026-03-01 22:32:25 -0500

    searchBar_header

---
+++ src/components/searchBar_header.jsx
@@ -0,0 +1,10 @@
+const searchBar_header = () => {
+    return(
+    <div className="searchBar_header">
+      <input type="text" placeholder="Buscar..." />
+      <button>🔍</button>
+    </div>
+    )
+}
+
+export default searchBar_header
