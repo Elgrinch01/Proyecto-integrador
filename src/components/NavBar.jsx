44ec30f59b233e071a2d96d5cc31d0ff034b446b
Elgrinch01 <sbohorquez@cesde.net>
2026-03-02 13:22:16 -0500

    Delete NavBar

--- src/components/NavBar.jsx
+++
@@ -1,7 +0,0 @@
-function NavBar() {
-
-}
-
-export default NavBar
-
-//Funcion placeholder para la barra de navegación, se puede agregar cualquier contenido que se quiera mostrar en la barra de navegación.
