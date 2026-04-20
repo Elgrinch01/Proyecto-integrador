8c899cc5340b0f17c7d7095fd17c8a389e35b7da
Malzate1 <nadja.minni@gmail.com>
2026-04-19 21:08:02 -0500

    Replace RouterProvider with BrowserRouter and App

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import './index.css';
-
-
-import { routerApp } from "../src/router/routerApp"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routerApp} />
-  </StrictMode>
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
