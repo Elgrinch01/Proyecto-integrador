3cb0d7830c64452406842abba69f800afd030144
Elgrinch01 <sbohorquez@cesde.net>
2026-04-20 16:22:29 -0500

    HU05-HUO6-Index-Mejora en css

--- src/components/NavBarHeader.jsx
+++ src/components/NavBarHeader.jsx
@@ -1,27 +1,35 @@
 import { useNavigate } from "react-router-dom";
 
-const NavBarHeader = () => {
+const NavBarHeader = ({ hideButtons = false, showSearch = false }) => {
   const navigate = useNavigate();
 
   return (
     <aside className="nav-bar-header">
       <img src="/src/assets/Logo.png" alt="Logo" />
 
-      <nav>
-        <button
-          className="nav-btn"
-          onClick={() => navigate("/reserva")}
-        >
-          Reservar
-        </button>
+      {showSearch && (
+        <div className="nav-bar-search">
+          <input type="text" placeholder="Busca tu libro favorito" />
+        </div>
+      )}
 
-        <button
-          className="nav-btn"
-          onClick={() => navigate("/login")}
-        >
-          Iniciar Sesion
-        </button>
-      </nav>
+      {!hideButtons && (
+        <nav>
+          <button
+            className="nav-btn"
+            onClick={() => navigate("/reserva")}
+          >
+            Reservar
+          </button>
+
+          <button
+            className="nav-btn"
+            onClick={() => navigate("/login")}
+          >
+            Iniciar Sesion
+          </button>
+        </nav>
+      )}
     </aside>
   );
 };
